2f446e882e51a6e2c47f094e539a7006299e09fd
PrivateHistory <70471875+PrivateHistory@users.noreply.github.com>
2021-01-27 13:32:10 +0200

    Add files via upload

---
+++ Position.py
@@ -0,0 +1,21 @@
+Last_left=0
+Last_Right=0
+x=0
+y=0
+def position():
+    #Get encoder values
+    Left=robot.get_motor_encoder(robot.MOTOR_LEFT)-Last_Left
+    Right=robot.get_motor_encoder(robot.MOTOR_RIGHT)-Last_Right
+    #Get distance from encoer 
+    distance=min(Left,Right)*WHEEL_CIRCUMFERENCE/360
+    #Save last values
+    Last_Left=robot.get_motor_encoder(robot.MOTOR_LEFT)
+    Right=robot.get_motor_encoder(robot.MOTOR_RIGHT)-Last_Right
+    #Get angle
+    angle=(Right-Left)%360
+    #Get distances
+    x=x+distance*math.cos(angle)
+    y=y+distance*math.sin(angle)
+    return [x,y]
+    
+    
